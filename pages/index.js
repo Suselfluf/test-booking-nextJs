199da7c4f184febcbc71e73eab93cde363f5a18f
SuselFulf <danya.lyagin@gmail.com>
2021-03-26 20:22:29 +0300

    Individualized nav panel to single component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import NavPanel from "./api/navPanel";
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.urls}>
-          <a>Home</a><a>Schedule</a><a>About</a>
-      </div>
-      <div className={styles.btns}>
-          <div id={styles.bookingBtn}><h6>Booking</h6></div>
-          <div id={styles.contactBtn}><h6>Contact us</h6></div>
-      </div>
-
-    </div>
+      <NavPanel></NavPanel>
   )
 }
