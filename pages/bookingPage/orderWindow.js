51bf170fc417a1ae9dd5f6045f708e740c0db507
SuselFulf <danya.lyagin@gmail.com>
2021-11-10 18:05:28 +0300

    Added TimeOption for variative displaying options, available for reservation

--- pages/bookingPage/orderWindow.js
+++ pages/bookingPage/orderWindow.js
@@ -2,7 +2,7 @@ import styles from '../../styles/orderWindow.module.sass'
 import React from 'react'
 import Calendar from 'react-calendar';
 import {reservingForm} from "../forms/reservingForm";
-import {TimeTable} from "./TimeTable";
+import {TimeOption} from "./TimeOption";
 
 
 export class OrderWindow extends React.Component{
@@ -10,14 +10,16 @@ export class OrderWindow extends React.Component{
         super(props)
         this.state = {
             marked: false,
-            isTimeOpen:false
+            isTimeOpen:false,
+            reservedTime:"null"
         }
         this.choose = 0;
         this.data = new reservingForm();
-        this.response = null;
+        this.res = 'null';
         this.day = null;
 
     }
+
 
 
 
@@ -28,8 +30,6 @@ export class OrderWindow extends React.Component{
 
         this.data.setData(this.choose[2], this.choose[1], this.choose[3]);
 
-        console.log('Request has started')
-
 
         await fetch('/api/inc', {
             method: 'post',
@@ -39,18 +39,28 @@ export class OrderWindow extends React.Component{
                 'Accept': 'application/json'
             },
         })
-            .then(res => res.json().then(data => console.log(data)));
-
+            .then(res => res.json().then(data => {
+                // this.openTime(data)
+                this.res = data
+                for (const resKey of data) {
+                    console.log(resKey.Date)
+                }
+                const res = JSON.stringify(data)
+                console.log(JSON.parse(res))
+                this.openTime(JSON.stringify(data))
+            }));
         // get String with reservation info
-        this.data.getData();
+        // this.data.getData();
 
 
     }
 
-    openTime = async (date) =>{
+    openTime = async (data) =>{
         this.setState({
-            isTimeOpen:true
+            isTimeOpen:true,
+            reservedTime:data
         });
+
     }
 
 
@@ -58,6 +68,12 @@ export class OrderWindow extends React.Component{
     render(){
 
         const {currentTableNumber, closeTab} = this.props
+        const TimeArray = [
+            {id:1,time:"12:00"},
+            {id:2,time:"15:00"},
+            {id:3,time:"18:00"},
+            {id:4,time:"21:00"}
+        ];
 
         return(
             <>
@@ -72,15 +88,21 @@ export class OrderWindow extends React.Component{
                         <p>Table number: {currentTableNumber}</p>
                         <h1>Choose date of your visit</h1>
                         <Calendar
-                            onChange={(value) => this.sendFetch(value)}
-                            onClickDay={(day) => this.openTime(day)}
+                            onClickDay={(day) => {
+                                this.sendFetch(day)
+                                // this.openTime(day)
+
+                            }}
                         />
                     </div>
 
                     <div className={styles.choice}>
                         <p>Chosen date: </p>
                         {<p>{this.day}</p>}
-                        {this.state.isTimeOpen ? <TimeTable/>: null}
+                        {this.state.isTimeOpen ? TimeArray.map(item => (
+                                <div key = {item.id} className={styles.timeTableCase}><TimeOption time = {item.time}> </TimeOption></div>
+                            )): null}
+
                     </div>
 
                 </div>
